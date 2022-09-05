b07718e7462fdf0c77733d4c34491759a694d16e
Hackerprod <hackerprodlive@gmail.com>
2022-09-05 18:26:48 +0200

    Update

---
+++ [SKYNET] MK Releaser/Helpers/Common.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+public class Common
+{
+    public static void Show(object msg)
+    {
+        MessageBox.Show(msg.ToString());
+    }
+
+    public static string LongToMbytes(long lBytes)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        string str1 = "Bytes";
+        if (lBytes > 1024L)
+        {
+            string str2;
+            float num;
+            if (lBytes < 1048576L)
+            {
+                str2 = "KB";
+                num = Convert.ToSingle(lBytes) / 1024f;
+            }
+            else
+            {
+                str2 = "MB";
+                num = Convert.ToSingle(lBytes) / 1048576f;
+            }
+            stringBuilder.AppendFormat("{0:0.0} {1}", (object)num, (object)str2);
+        }
+        else
+        {
+            float num = Convert.ToSingle(lBytes);
+            stringBuilder.AppendFormat("{0:0} {1}", (object)num, (object)str1);
+        }
+        return stringBuilder.ToString();
+    }
+
+}
---
+++ [SKYNET] MK Releaser/Helpers/Settings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SKYNET.Helpers
+{
+    public class Settings
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        public static string Host { get; set; }
+        public static bool AutoConnect { get; set; }
+        public static bool AutoRelease { get; set; }
+        
+
+
+        private static RegistrySettings Registry;
+
+        static Settings()
+        {
+            Registry = new RegistrySettings(@"SOFTWARE\SKYNET\[SKYNET] MK Releaser\");
+        }
+
+        public static void Load()
+        {
+            Username = Registry.Get<string>("Username", "admin");
+            Password = Registry.Get<string>("Password", "");
+            Host = Registry.Get<string>("Host", "192.168.88.1");
+            AutoConnect = Registry.Get<bool>("AutoConnect", false);
+            AutoRelease = Registry.Get<bool>("AutoRelease", false);
+        }
+
+        public static void Save()
+        {
+            Registry.Set("Username", Username);
+            Registry.Set("Password", Password);
+            Registry.Set("Host", Host);
+            Registry.Set("AutoConnect", AutoConnect);
+            Registry.Set("AutoRelease", AutoRelease);
+            
+        }
+    }
+}
+
